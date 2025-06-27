689a0e558b7c2349faeec4e6cb53b531bd105613
Mumin2612 <muminsulaymonov5@gmail.com>
2025-06-27 22:02:06 +0200

    Update kossa.py

--- kossa.py
+++ kossa.py
@@ -185,4 +185,9 @@ def index():
 def run_bot():
     bot.infinity_polling()
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+else:
+    threading.Thread(target=run_bot).start()
+
 
